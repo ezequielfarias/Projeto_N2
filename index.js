bdf10257834cfd5ad49ed65a6f1ff7cd9b6512d5
Ezequiel Adriel Leite  Farias <78233866+ezequielfarias@users.noreply.github.com>
2024-09-10 13:53:30 -0300

    atualizando para metas

--- index.js
+++ index.js
@@ -1,49 +1,88 @@
-const{select, input} = require('@inquirer/prompts')
-const CadastrarCamera = async () =>{
-  const camera = await input({message:" Digite a descrição da Câmera"})
+const { select, input, checkbox } = require('@inquirer/prompts')
 
-    if (camera.length == 0){
-      console.log("A descrição não pode ser vazia!")
-      return 
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta:"})
+
+    if(meta.length == 0) {
+        console.log('A meta não pode ser vazia.')
+        return
+    }
+
+    metas.push(
+        { value: meta, checked: false }
+    )
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
+
+}
+
+
+const start = async () => {
+    while(true){
+        
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch(opcao) {
+            case "cadastrar":
+                await cadastrarMeta()
+                console.log(metas)
+                break
+            case "listar":
+                await listarMetas()
+                break
+            case "sair":
+                console.log('Até a próxima!')
+                return
+        }
     }
 }
 
-
-const start = async ()=>{
-  while(true){
-    const opcao = await select({
-      message:"MENU",
-      choices:[
-        {
-          name: "Cadastrar Câmera",
-          value: "Cadastrar"
-        },
-        {
-          name: "Listar Câmeras",
-          value: "Listar"
-        },
-        {
-          name: "sair",
-          value: "sair"
-        },
-      ]
-    })
-
-  switch (opcao){
-      case "Cadastrar Câmera":
-        await CadastrarCamera()
-      console.log("Vamos cadastrar")
-      break
-
-      case "Listar Câmeras":
-          console.log("Vamos listar")
-          break
-          case "sair":
-            return
-}
-
-
-  }
-}
-
-start()
+start();
