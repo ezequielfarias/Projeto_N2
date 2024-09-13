e10bb19ed5f3ffbc5cfac87f739113fd7855deeb
Ezequiel Adriel Leite  Farias <78233866+ezequielfarias@users.noreply.github.com>
2024-09-13 10:11:08 -0300

    atualizando

--- my-electron-app/main.js
+++
@@ -1,8 +0,0 @@
-const { app, BrowserWindow } = require('electron')
-createWindow()
-app.whenReady().then(() => {
-  createWindow()
-})
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
